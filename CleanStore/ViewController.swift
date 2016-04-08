3a75f4ddb1e58646a41d5846d333d55d3e6657f3
Waratnan Suriyasorn <s.waratnan@gmail.com>
2016-04-08 10:13:53 +0700

    Initial Commit

---
+++ CleanStore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CleanStore
+//
+//  Created by Waratnan Suriyasorn on 4/8/2559 BE.
+//  Copyright © 2559 M Warat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
